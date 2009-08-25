b2ed04ca1a74e1e9dc9374486cdbf915ad8b9402
Miguel Regedor <regedor@gavetas.(none)>
2009-08-25 03:57:48 +0100

    Correction on mail check system

--- site_files/app/models/user_mailer.rb
+++ site_files/app/models/user_mailer.rb
@@ -1,23 +1,21 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Please activate your new account'
-  
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
-  
+    @subject    += 'Confirmação do endereço de email'
+    @body[:url]  = "http://ism.itacaproject.com/activate/#{user.activation_code}"
   end
   
   def activation(user)
     setup_email(user)
-    @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @subject    += 'A seu registo está confirmado!'
+    @body[:url]  = "http://ism.itacaproject.com/"
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "Imagens e Sonoriades das Migrações"
+      @subject     = "[ism.itacaproject.com] "
       @sent_on     = Time.now
       @body[:user] = user
     end
